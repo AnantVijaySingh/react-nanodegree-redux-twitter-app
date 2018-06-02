b24d30f96b8e5480e410727f6521f08f05638e4d
anantvijaysingh <anantvijay.23@gmail.com>
2018-06-02 17:51:49 +0100

    Using connect to create a container component for the UI component Connect takes in the state and creates a container component that uses context to get the store and pass the dispatch methods and the store to the standard UI component as porps that will then render it.

--- src/components/App.js
+++ src/components/App.js
@@ -17,3 +17,7 @@ class App extends Component {
 }
 
 export default connect()(App);
+
+// connect helps create a container/connected component that uses context to get the store using Provider/Context.Consumer component pair
+// the first argument takes in the state that needs to be passed as props and the chained function takes in the component that needs to be passed this state along with the dispatch functionality
+// this second function arguments represents the UI/Presentational component that will take in the state and dispatch and display the required UI
